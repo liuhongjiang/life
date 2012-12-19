5277548c985b4eba2b0c9a9d247e2da44e60567a
liuhongjiang <andrew.lhj@gmail.com>
2012-12-19 15:51:14 +0800

    up

--- plugins/tag_cloud.rb
+++ plugins/tag_cloud.rb
@@ -96,7 +96,8 @@ module Jekyll
     def render(context)
       html = ""
       config = context.registers[:site].config
-      category_dir = config['root'] + config['category_dir'] + '/'
+     # category_dir = config['root'] + config['category_dir'] + '/'
+      category_dir = '/' + config['category_dir'] + '/'
       categories = context.registers[:site].categories
       categories.keys.sort_by{ |str| str.downcase }.each do |category|
         url = category_dir + category.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-').downcase
